d469ac086a6747465d1038ef0d587572a7c6a0c3
omarriyaz <or2000@hw.ac.uk>
2024-02-19 20:45:34 +0000

    PDF Upload Working

--- Code/app.py
+++ Code/app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from streamlit_extras.add_vertical_space import add_vertical_space
+from PyPDF2 import PdfReader
 
 # sidebar
 with st.sidebar:
@@ -13,3 +14,16 @@ with st.sidebar:
     ''')
     add_vertical_space(5)
     st.write('References: [Prompt Engineer](https://youtube.com/@engineerprompt)')
+
+def main():
+    st.header("Chat with the Teaching Assistant Chatbot")
+    
+    # upload a PDF file
+    pdf = st.file_uploader("Upload your PDF Here", type="pdf")
+
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+
+
+if __name__ == "__main__":
+    main()
