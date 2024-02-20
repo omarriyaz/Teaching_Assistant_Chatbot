4ebfe67f9c8885cbc9fcc5327c5d14ef9e123e1f
omarriyaz <or2000@hw.ac.uk>
2024-02-20 20:45:58 +0000

    Changed Structure of the Code, added a .gitignore and .env with my API keys

--- Code/app.py
+++ Code/app.py
@@ -1,29 +1,37 @@
 import streamlit as st
-from streamlit_extras.add_vertical_space import add_vertical_space
+from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 
-# sidebar
-with st.sidebar:
-    st.title('LLM Chatbot')
-    st.markdown('''
-    This app is an LLM-powered chatbot built using:
-    - [Streamlit](https://streamlit.io/)
-    - [LangChain](https://python.langchain.com/)
-    - [OpenAI](https://platform.openai.com/docs/models) LLM Model
-    
-    ''')
-    add_vertical_space(5)
-    st.write('References: [Prompt Engineer](https://youtube.com/@engineerprompt)')
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
 
 def main():
+    load_dotenv() # gives access to API keys
+
+    st.set_page_config(page_title="Teaching Assistant Chatbot")
+
     st.header("Chat with the Teaching Assistant Chatbot")
-    
-    # upload a PDF file
-    pdf = st.file_uploader("Upload your PDF Here", type="pdf")
+    st.text_input("Ask me a question about your documents:")
 
-    if pdf is not None:
-        pdf_reader = PdfReader(pdf)
+    with st.sidebar:
+        st.subheader("Your documents")
+        pdf_docs = st.file_uploader("Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+        
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                # get pdf text
+                raw_text = get_pdf_text(pdf_docs)
 
+                # get the text chunks
+                text_chunks = get_text_chunks(raw_text)
+
+                # create vector store
+                vectorstore = get_vectorstore(text_chunks)
 
 if __name__ == "__main__":
     main()
