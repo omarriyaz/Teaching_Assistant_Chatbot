9b7450121e43b7d444a0a2151ea774ec76975e3a
omarriyaz <or2000@hw.ac.uk>
2024-02-23 00:31:36 +0000

    Working Chatbot

--- Code/app.py
+++ Code/app.py
@@ -1,29 +1,90 @@
 import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.llms import HuggingFaceHub
+from htmlTemplates import css, bot_template, user_template
 
 def get_pdf_text(pdf_docs):
     text = ""
     for pdf in pdf_docs:
-        pdf_reader = PdfReader(pdf)
+        pdf_reader = PdfReader(pdf) # using the PdfReader class from PyPDF2 library
         for page in pdf_reader.pages:
             text += page.extract_text()
     return text
 
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter( # using the CharacterTextSplitter class from langchain library
+        separator="\n", # single line break
+        chunk_size=1000, # chunk size of 1000 characters
+        chunk_overlap=200, # chunk overlap of 200 characters 
+        length_function=len # length
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def get_vectorstore(text_chunks):
+    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl") # instructor-xl embeddings
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
+def get_conversation_chain(vectorstore):
+    
+    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512}) 
+
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm, # flan-t5-xxl
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
+def handle_user_input(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+
+
 def main():
     load_dotenv() # gives access to API keys
 
-    st.set_page_config(page_title="Teaching Assistant Chatbot")
+    st.set_page_config(page_title="Teaching Assistant Chatbot") # page title
 
-    st.header("Chat with the Teaching Assistant Chatbot")
-    st.text_input("Ask me a question about your documents:")
+    st.write(css, unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
+    st.header("Chat with the Teaching Assistant Chatbot") # header
+    user_question = st.text_input("Ask me a question about your documents:") # text input where the user can ask their questions
+    if user_question:
+        handle_user_input(user_question)
+
+    
+    # sidebar
 
     with st.sidebar:
         st.subheader("Your documents")
         pdf_docs = st.file_uploader("Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
         
-        if st.button("Process"):
-            with st.spinner("Processing"):
+        if st.button("Process"): # process button
+
+            with st.spinner("Processing"): # 'Processing' spinner
+
                 # get pdf text
                 raw_text = get_pdf_text(pdf_docs)
 
@@ -32,6 +93,9 @@ def main():
 
                 # create vector store
                 vectorstore = get_vectorstore(text_chunks)
+
+                # create conversation chain
+                st.session_state.conversation = get_conversation_chain(vectorstore) # Makes conversation persistent (only lasts for the session of the application)
 
 if __name__ == "__main__":
     main()
